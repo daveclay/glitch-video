a4ef67b0fe1b0506fcafac6ebfeb8e684340c354
daveclay <daveclay@gmail.com>
2013-09-20 15:29:43 -0400

    adding a stupid video player  applet that at least plays as many frames as possible, though the rendering is slow

--- src/main/scala/net/retorx/glitchvideo/Main.scala
+++ src/main/scala/net/retorx/glitchvideo/Main.scala
@@ -4,37 +4,17 @@ import com.xuggle.mediatool.{IMediaReader, MediaToolAdapter, ToolFactory}
 import java.awt.image.{Raster, BufferedImage}
 import com.xuggle.mediatool.event.IVideoPictureEvent
 import java.util.{Date, Random}
-import net.retorx.glitchvideo.player.{PlayMedia, Player}
+import net.retorx.glitchvideo.player.{PlayerUI, PlayMedia, Player}
 import PlayMedia._
-import net.retorx.glitchvideo.player.Player
 
 object Main {
 
     def main(args: Array[String]) = {
-        val random = new Random(new Date().getTime)
-        val outputFilename = "/Users/daveclay/SYRIA/blah" + random.nextInt(1000) + ".mov"
         //glitch("videos/WHUT_20100602_020000_Tavis_Smiley.mp4?t=1798%2F1828&ignore=x.mp4", "blah.mov")
         //glitch("videos/CSPAN_20120707_212500_Gun_Rights_Case_Oral_Argument.mp4?t=108%2F138&ignore=x.mp4", outputFilename)
-        glitch("videos/CSPAN2_20120828_103000_Book_TV.mp4?t=1795%2F1825&ignore=x.mp4", outputFilename)
+        val player = new PlayerUI("videos/CSPAN2_20120828_103000_Book_TV.mp4?t=1795%2F1825&ignore=x.mp4")
+        player.play()
         //glitch("videos/CNNW_20121222_080000_Piers_Morgan_Tonight.mp4?t=119%2F149&ignore=x.mp4", outputFilename)
-    }
-
-    def glitch(inputFilename: String, outputFilename: String) {
-        glitchVideo(inputFilename, outputFilename)
-        Player.play(outputFilename)
-    }
-
-    def glitchVideo(inputFilename: String, outputFilename: String) {
-        val mediaReader = ToolFactory.makeReader(inputFilename)
-        mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR)
-
-        val mediaWriter = ToolFactory.makeWriter(outputFilename, mediaReader)
-
-        val glitcher = new GlitcherTool()
-        mediaReader.addListener(glitcher)
-        glitcher.addListener(mediaWriter)
-
-        play(mediaReader)
     }
 }
 
---
+++ src/main/scala/net/retorx/glitchvideo/player/FasterPlayer.scala
@@ -0,0 +1,93 @@
+package net.retorx.glitchvideo.player
+
+import java.awt.{Graphics, Dimension, Component}
+import java.awt.image.BufferedImage
+import javax.swing.{JPanel, Timer, JFrame, JApplet}
+import java.awt.event.{ActionEvent, ActionListener, WindowEvent, WindowAdapter}
+import java.net.{MalformedURLException, URL}
+import java.io.File
+import com.xuggle.mediatool.{ToolFactory, MediaToolAdapter}
+import com.xuggle.mediatool.event.IVideoPictureEvent
+import net.retorx.glitchvideo.GlitcherTool
+import java.util.{Date, Random}
+
+class PlayerUI(filename: String) {
+
+    val random = new Random(new Date().getTime)
+    val outputFilename = "/Users/daveclay/SYRIA/blah" + random.nextInt(1000) + ".mov"
+
+    def play() {
+        val jframe = new JFrame("Player")
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+
+        val imageSource = new ImageSourceAdapter()
+        val videoComponent = new VideoImageComponent(649, 480, imageSource)
+        jframe.setContentPane(videoComponent)
+        jframe.pack()
+        jframe.setVisible(true)
+
+        videoComponent.animate()
+
+        val glitchPlayer = new GlitchPlayer(imageSource)
+        glitchPlayer.glitch(filename, outputFilename)
+    }
+}
+
+class VideoImageComponent(width: Int, height: Int, imageSource: ImageSource) extends JPanel {
+
+    setPreferredSize(new Dimension(width, height))
+
+    def animate() {
+        val framesPerSecond = 30
+        val delay = 1000 / framesPerSecond
+        val timer = new Timer(delay, null)
+
+        timer.addActionListener(new ActionListener {
+            def actionPerformed(e: ActionEvent) {
+                repaint()
+            }
+        })
+
+        timer.start()
+    }
+
+    override def paintComponent(graphics: Graphics) {
+        val image = imageSource.getImage
+        if (image != null) {
+            graphics.drawImage(image, 0, 0, null)
+        }
+    }
+
+}
+
+trait ImageSource {
+    def getImage: BufferedImage
+}
+
+class ImageSourceAdapter extends MediaToolAdapter with ImageSource {
+
+    var image: BufferedImage = null
+
+    def getImage: BufferedImage = image
+
+    override def onVideoPicture(event: IVideoPictureEvent) {
+        image = event.getImage
+        super.onVideoPicture(event)
+    }
+}
+
+class GlitchPlayer(imageSourceAdapter: ImageSourceAdapter) {
+
+    def glitch(inputFilename: String, outputFilename: String) {
+        val mediaReader = ToolFactory.makeReader(inputFilename)
+        mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR)
+
+        val mediaWriter = ToolFactory.makeWriter(outputFilename, mediaReader)
+
+        val glitcher = new GlitcherTool()
+        mediaReader.addListener(glitcher)
+        glitcher.addListener(imageSourceAdapter)
+        imageSourceAdapter.addListener(mediaWriter)
+        PlayMedia.play(mediaReader)
+    }
+}
